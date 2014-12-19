5df1c55efa67e00c4691f4b053b495298caf61c1
Ben Steinberg <benjamin_steinberg@harvard.edu>
2014-12-19 12:03:30 -0500

    Add refresh button

--- upteele.py
+++ upteele.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from flask_bootstrap import Bootstrap
 import xml.etree.ElementTree as ET
 import requests
